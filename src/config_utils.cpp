48a443aec8d90b9b29860b97c0f37867a1931575
Brian S. Stephan <bss@incorporeal.org>
2024-04-15 18:09:22 -0500

    board config defaults: bootsel button

--- src/config_utils.cpp
+++ src/config_utils.cpp
@@ -683,10 +683,6 @@ void ConfigUtils::initUnsetPropertiesWithDefaults(Config& config)
         }
     }
 
-    // addonOptions.bootselButtonOptions
-    INIT_UNSET_PROPERTY(config.addonOptions.bootselButtonOptions, enabled, !!BOOTSEL_BUTTON_ENABLED);
-    INIT_UNSET_PROPERTY(config.addonOptions.bootselButtonOptions, buttonMap, BOOTSEL_BUTTON_MASK);
-
     // addonOptions.onBoardLedOptions
     INIT_UNSET_PROPERTY(config.addonOptions.onBoardLedOptions, enabled, !!BOARD_LED_ENABLED);
     INIT_UNSET_PROPERTY(config.addonOptions.onBoardLedOptions, mode, BOARD_LED_TYPE);
