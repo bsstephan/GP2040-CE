7ae88b5c1d1854e6a7ecce5c463929eeada585da
Brian S. Stephan <bss@incorporeal.org>
2026-05-04 09:01:33 -0500

    add methods for writing runtime config to flash

--- lib/FlashPROM/src/FlashPROM.cpp
+++ lib/FlashPROM/src/FlashPROM.cpp
@@ -8,6 +8,7 @@
 #include "FlashPROM.h"
 
 uint8_t FlashPROM::userConfigWriteCache[UCONFIG_EEPROM_SIZE_BYTES];
+uint8_t FlashPROM::runtimeConfigWriteCache[RCONFIG_EEPROM_SIZE_BYTES];
 volatile static alarm_id_t flashWriteAlarm = 0;
 volatile static spin_lock_t *flashLock = nullptr;
 
@@ -31,12 +32,41 @@ int64_t writeUserConfigToFlash(alarm_id_t id, void *flashCache)
 	return 0;
 }
 
+/* sorry for the copy and paste job here, I can't DRY this easily because of the interface for add_alarm_in_ms ~bss */
+int64_t writeRuntimeConfigToFlash(alarm_id_t id, void *flashCache)
+{
+	while (is_spin_locked(flashLock));
+
+	multicore_lockout_start_blocking();
+	uint32_t interrupts = spin_lock_blocking(flashLock);
+
+	flash_range_erase((intptr_t)RCONFIG_EEPROM_ADDRESS_START -
+			(intptr_t)XIP_BASE, RCONFIG_EEPROM_SIZE_BYTES);
+	flash_range_program((intptr_t)RCONFIG_EEPROM_ADDRESS_START -
+			(intptr_t)XIP_BASE, reinterpret_cast<uint8_t *>(flashCache), RCONFIG_EEPROM_SIZE_BYTES);
+
+	flashWriteAlarm = 0;
+
+	multicore_lockout_end_blocking();
+	spin_unlock(flashLock, interrupts);
+
+	return 0;
+}
+
 void FlashPROM::userConfigStart()
 {
 	if (flashLock == nullptr)
 		flashLock = spin_lock_instance(spin_lock_claim_unused(true));
 
 	memcpy(userConfigWriteCache, reinterpret_cast<uint8_t *>(UCONFIG_EEPROM_ADDRESS_START), UCONFIG_EEPROM_SIZE_BYTES);
+}
+
+void FlashPROM::runtimeConfigStart()
+{
+	if (flashLock == nullptr)
+		flashLock = spin_lock_instance(spin_lock_claim_unused(true));
+
+	memcpy(runtimeConfigWriteCache, reinterpret_cast<uint8_t *>(RCONFIG_EEPROM_ADDRESS_START), RCONFIG_EEPROM_SIZE_BYTES);
 }
 
 /* We don't have an actual EEPROM, so we need to be extra careful about minimizing writes. Instead
@@ -50,8 +80,22 @@ void FlashPROM::userConfigCommit()
 	flashWriteAlarm = add_alarm_in_ms(EEPROM_WRITE_WAIT, writeUserConfigToFlash, userConfigWriteCache, true);
 }
 
+void FlashPROM::runtimeConfigCommit()
+{
+	while (is_spin_locked(flashLock));
+	if (flashWriteAlarm != 0)
+		cancel_alarm(flashWriteAlarm);
+	flashWriteAlarm = add_alarm_in_ms(EEPROM_WRITE_WAIT, writeRuntimeConfigToFlash, runtimeConfigWriteCache, true);
+}
+
 void FlashPROM::userConfigReset()
 {
 	memset(userConfigWriteCache, 0, UCONFIG_EEPROM_SIZE_BYTES);
 	userConfigCommit();
+}
+
+void FlashPROM::runtimeConfigReset()
+{
+	memset(runtimeConfigWriteCache, 0, RCONFIG_EEPROM_SIZE_BYTES);
+	runtimeConfigCommit();
 }
--- lib/FlashPROM/src/FlashPROM.h
+++ lib/FlashPROM/src/FlashPROM.h
@@ -14,6 +14,8 @@
 
 #define UCONFIG_EEPROM_SIZE_BYTES    0x8000		// Reserve 32k of flash memory (ensure this value is divisible by 256)
 #define UCONFIG_EEPROM_ADDRESS_START _u(0x101F8000)
+#define RCONFIG_EEPROM_SIZE_BYTES    0x0100		// 256 bytes
+#define RCONFIG_EEPROM_ADDRESS_START _u(0x101F7F00)
 
 // Warning: If the write wait is too long it can stall other processes
 #define EEPROM_WRITE_WAIT    50             // Amount of time in ms to wait before blocking core1 and committing to flash
@@ -24,8 +26,12 @@ class FlashPROM
 		void userConfigStart();
 		void userConfigCommit();
 		void userConfigReset();
+		void runtimeConfigStart();
+		void runtimeConfigCommit();
+		void runtimeConfigReset();
 
 		static uint8_t userConfigWriteCache[UCONFIG_EEPROM_SIZE_BYTES];
+		static uint8_t runtimeConfigWriteCache[RCONFIG_EEPROM_SIZE_BYTES];
 };
 
 inline FlashPROM EEPROM;
